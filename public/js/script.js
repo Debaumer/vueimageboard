4ec955a9f78ccf6832a5f79aa24e237d5cfe834f
joshua doolan <joshuadoolan11@gmail.com>
2019-02-26 11:19:22 +0100

    end of day41 first encounter example code

--- public/js/script.js
+++ public/js/script.js
@@ -11,12 +11,37 @@ var hello = "this is my greeting";
 
 (function() {
     new Vue({
-        el: "#upload",
-        data: {},
+        el: "#uploadForm",
+        data: {
+            name: "hello",
+            form: {
+                title: "",
+                username: "",
+                description: "",
+                file: null
+            }
+        },
         methods: {
             upload: function(e) {
-                console.log("upload started");
+                console.log("upload started", "event: " + e, "this: ", this);
+                console.log(this.form.description);
+                e.preventDefault();
+                var formData = new FormData();
+                formData.append("file", this.form.file);
+                formData.append("title", this.form.tile);
+                formData.append("description", this.form.description);
+                formData.append("username", this.form.username);
+                //console.log(formData); this is always empty, but append does work
+                axios.post("/upload", formData);
+            },
+            getImg: function(e) {
+                this.form.file = e.target.files[0];
+                console.log(this.form.file);
             }
+        },
+        created: function() {
+            console.log("hello");
+            console.log(this);
         }
     });
 })();
@@ -32,12 +57,10 @@ var hello = "this is my greeting";
             axios
                 .get("/imgpath")
                 .then(function(resp) {
-                    console.log(resp.data);
                     self.items = resp.data;
-                    console.log(self.items);
                 })
                 .catch(function(err) {
-                    console.log(err);
+                    console.log("ERROR", err);
                 });
         }
         // created: ,
--- server.js
+++ server.js
@@ -3,6 +3,41 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 app.use(express.static("./public"));
+var multer = require("multer");
+var uidSafe = require("uid-safe");
+var path = require("path");
+
+app.use(express.static(__dirname + "/public"));
+
+var diskStorage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function(req, file, callback) {
+        uidSafe(24).then(function(uid) {
+            callback(null, uid + path.extname(file.origionalname));
+        });
+    }
+});
+
+var uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152
+    }
+});
+
+app.post("/upload", uploader.single("file"), function(req, res) {
+    if (req.file) {
+        res.json({
+            success: true
+        });
+    } else {
+        res.json({
+            success: false
+        });
+    }
+});
 
 app.get("/imgpath", (req, res) => {
     console.log("you did it");
